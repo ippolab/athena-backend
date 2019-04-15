0cebbe9640d2ddc60cfab496bb384c56b70a1cb1
Slyfoxy <khitrov34@gmail.com>
2019-04-15 23:46:38 +0300

    add permissions to views in edu and reports view

--- athena/core/storage.py
+++ athena/core/storage.py
@@ -1,5 +1,4 @@
 import os
-import pathlib
 
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
@@ -10,12 +9,3 @@ class OverwriteStorage(FileSystemStorage):
         if self.exists(name):
             os.remove(os.path.join(settings.MEDIA_ROOT, name))
         return name
-
-
-def compare_dirs(path: str, filename: str) -> bool:
-    try:
-        abspath = pathlib.Path(settings.MEDIA_ROOT)
-        abspath /= filename
-        return os.path.samefile(abspath, path)
-    except FileNotFoundError:
-        return False
