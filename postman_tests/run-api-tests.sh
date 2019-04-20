f353ef985cf24bf250e45d4945cbcefef14ef26b
Slyfoxy <khitrov34@gmail.com>
2019-04-20 14:26:51 +0300

    rename api url var

--- postman_tests/run-api-tests.sh
+++ postman_tests/run-api-tests.sh
@@ -9,7 +9,7 @@ PASSWORD=${ATHENA_ADMIN_PASSWORD:-admin}
 
 npx newman run ${SCRIPTDIR}/Athena.postman_collection.json \
   --delay-request 500 \
-  --global-var "domain=$APIURL" \
+  --global-var "apiUrl=$APIURL" \
   --global-var "admin_username=$USERNAME" \
   --global-var "admin_password=$PASSWORD" \
 
