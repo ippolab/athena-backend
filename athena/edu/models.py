fba752aa6dc301b26c052f152a49c91495320e87
Slyfoxy <khitrov34@gmail.com>
2019-05-07 02:09:50 +0300

    add validators to Speciality, Subject and  StudentGroup

--- athena/edu/models.py
+++ athena/edu/models.py
@@ -1,19 +1,14 @@
+from django.core import validators
 from django.db import models
 
 from athena.core.models import UUIDModel
 
 
-class Speciality(UUIDModel):
-    cipher = models.CharField(max_length=15, unique=True)
-    name = models.CharField(max_length=64, unique=True)
-
-    def __str__(self):
-        return "{}. {}".format(self.cipher, self.name)
-
-
 class Subject(UUIDModel):
     name = models.CharField(max_length=64)
-    semester = models.PositiveSmallIntegerField(default=1)
+    semester = models.PositiveSmallIntegerField(
+        default=1, validators=[validators.RegexValidator(r"[1-8]{1}")]
+    )
 
     class Meta:
         unique_together = ("name", "semester")
@@ -22,8 +17,26 @@ class Subject(UUIDModel):
         return "{} [{}]".format(self.name, self.semester)
 
 
+class Speciality(UUIDModel):
+    cipher = models.CharField(
+        max_length=8,
+        unique=True,
+        validators=[validators.RegexValidator(r"[\d{2}.\d{2}}.\d{2}")],
+    )
+    name = models.CharField(max_length=64, unique=True)
+    speciality = models.ManyToManyField(Subject, related_name="specialities")
+
+    def __str__(self):
+        return "{}. {}".format(self.cipher, self.name)
+
+
 class StudentGroup(UUIDModel):
-    name = models.CharField(max_length=32, unique=True)
+    name = models.CharField(
+        max_length=32,
+        unique=True,
+        validators=[validators.RegexValidator(r"[А-Яа-я]{4,6}-\d{2}-\d{2}")],
+    )
+
     speciality = models.ForeignKey(
         Speciality, related_name="groups", on_delete=models.CASCADE
     )
