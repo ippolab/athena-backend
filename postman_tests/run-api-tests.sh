ed683ee84420a95eec75c787a7d9bc8585ca7ecb
Nick <khitrov34@gmail.com>
2019-05-10 16:02:49 +0300

    update postman tests

--- postman_tests/run-api-tests.sh
+++ postman_tests/run-api-tests.sh
@@ -3,13 +3,13 @@ set -x
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-APIURL=${APIURL:-http://localhost:8000}
+APIURL=${APIURL:-http://localhost:8000/api}
 USERNAME=${ATHENA_ADMIN_USERNAME:-admin}
 PASSWORD=${ATHENA_ADMIN_PASSWORD:-admin}
 
 npx newman run ${SCRIPTDIR}/Athena.postman_collection.json \
   --delay-request 500 \
-  --global-var "domain=$APIURL" \
+  --global-var "apiUrl=$APIURL" \
   --global-var "admin_username=$USERNAME" \
   --global-var "admin_password=$PASSWORD" \
 
